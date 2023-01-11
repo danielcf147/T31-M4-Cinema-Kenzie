50e6497dfd794a11da6c5fd1403cfa32f1b27150
= <danielcf.vix@hotmail.com>
2023-01-11 17:42:11 -0300

    rev1 entities

--- src/entities/employeeEntity.ts
+++ src/entities/employeeEntity.ts
@@ -1,48 +1,57 @@
 import { hashSync } from "bcryptjs";
-import { BeforeInsert, BeforeUpdate, Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
 import { Order } from "./orderEntity";
 import { Room } from "./roomsEntity";
 
-@Entity('employees')
+@Entity("employees")
 export class Employee {
-    @PrimaryGeneratedColumn('uuid')
-    id: string
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
-    @Column()
-    name: string
+  @Column()
+  name: string;
 
-    @Column({unique: true})
-    registration: string
+  @Column({ unique: true })
+  registration: string;
 
-    @Column()
-    age: number
+  @Column()
+  age: number;
 
-    @Column()
-    isAdm: boolean
+  @Column()
+  isAdm: boolean;
 
-    @Column({default: true})
-    isActive: boolean
+  @Column({ default: true })
+  isActive: boolean;
 
-    @Column()
-    password: string
+  @Column()
+  password: string;
 
-    @Column()
-    createdAt: Date
+  @CreateDateColumn()
+  createdAt: Date;
 
-    @Column()
-    updatedAt: Date
+  @UpdateDateColumn()
+  updatedAt: Date;
 
-    @ManyToMany(() => Room, room => room.employee)
-    @JoinTable()
-    rooms: Room[]
+  @ManyToMany(() => Room, (room) => room.employee)
+  @JoinTable()
+  rooms: Room[];
 
-    @ManyToMany(() => Order, order => order.employee)
-    order: Order[]
-    
-    @BeforeUpdate()
-    @BeforeInsert()
-    hashPassword(){
-        this.password = hashSync(this.password, 10)
-    }
+  @ManyToMany(() => Order, (order) => order.employee)
+  order: Order[];
 
+  @BeforeUpdate()
+  @BeforeInsert()
+  hashPassword() {
+    this.password = hashSync(this.password, 10);
+  }
 }
