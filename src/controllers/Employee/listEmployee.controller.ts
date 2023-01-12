d24c2ff306163c050be8b8f870089afbca1812eb
= <danielcf.vix@hotmail.com>
2023-01-12 12:37:18 -0300

    middlewares created

--- src/controllers/Employee/listEmployee.controller.ts
+++ src/controllers/Employee/listEmployee.controller.ts
@@ -2,7 +2,7 @@ import listEmployeeService from "../../services/Employee/listEmployee.service";
 import { Request, Response } from "express";
 
 const listEmployeeController = async (request: Request, response: Response) => {
-  const isAdm = true;
+  const isAdm = request.user.isAdm;
   const listEmployee = await listEmployeeService(isAdm);
   return response.json(listEmployee);
 };
