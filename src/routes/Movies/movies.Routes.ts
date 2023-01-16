ca6478e1046dd599046e162b877e552fdef69d5c
Flipsy1 <felipevie@hotmail.com>
2023-01-16 11:19:28 -0400

    Serializer category, movies

--- src/routes/Movies/movies.Routes.ts
+++ src/routes/Movies/movies.Routes.ts
@@ -1,11 +1,22 @@
 import { Router } from "express";
-import { createMovieController, getAllMoviesController, getMovieByCategoryController, getMovieByIdController } from "../../controllers/Movies/movies.Controllers";
+import {
+  createMovieController,
+  getAllMoviesController,
+  getMovieByCategoryController,
+  getMovieByIdController,
+} from "../../controllers/Movies/movies.Controllers";
+import dataIsValid from "../../middlewares/dataIsValid.middleware";
+import { movieCreateSerializer } from "../../serializers/movies/movie.serializer";
 
-const moviesRouters = Router()
+const moviesRouters = Router();
 
-moviesRouters.post("", createMovieController)
-moviesRouters.get("", getAllMoviesController)
-moviesRouters.get("/:movieId", getMovieByIdController)
-moviesRouters.get("/:categoryId", getMovieByCategoryController)
+moviesRouters.post(
+  "",
+  dataIsValid(movieCreateSerializer),
+  createMovieController
+);
+moviesRouters.get("", getAllMoviesController);
+moviesRouters.get("/:movieId", getMovieByIdController);
+moviesRouters.get("/:categoryId", getMovieByCategoryController);
 
-export { moviesRouters }
+export { moviesRouters };
--- src/services/CategoryMovie/getMovieCategoryById.Service.ts
+++ src/services/CategoryMovie/getMovieCategoryById.Service.ts
@@ -1,23 +1,23 @@
-import AppDataSource from "../../data-source"
-import { CategoryMovie } from "../../entities/categoryMoviesEntity"
+import AppDataSource from "../../data-source";
+import { CategoryMovie } from "../../entities/categoryMoviesEntity";
 
 export async function getMovieCategoryByIdService(id: string) {
-    console.log(id)
-    const categoryRepository = AppDataSource.getRepository(CategoryMovie)
+  console.log(id);
+  const categoryRepository = AppDataSource.getRepository(CategoryMovie);
 
-    const categoryQueryBuilder = categoryRepository.createQueryBuilder("category")
+  const categoryQueryBuilder =
+    categoryRepository.createQueryBuilder("category");
 
-    const category = await categoryQueryBuilder
-        .leftJoinAndSelect("category.movies", "movie")
-        .where("category.id = :id", { id: id })
-        .getOne()
+  const category = await categoryQueryBuilder
+    .leftJoinAndSelect("category.movies", "movie")
+    .where("category.id = :id", { id: id })
+    .getOne();
 
-    const data = {
-        id: category.id,
-        name: category.name,
-        movies: category.movies
-    }
+  const data = {
+    id: category.id,
+    name: category.name,
+    movies: category.movies,
+  };
 
-    return data
-
+  return data;
 }
