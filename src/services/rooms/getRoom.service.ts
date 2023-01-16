d1d62abf4619f4e9d86da33a6c15e53678578997
Flipsy1 <felipevie@hotmail.com>
2023-01-16 15:47:25 -0400

    Correção rota room

--- src/services/rooms/getRoom.service.ts
+++ src/services/rooms/getRoom.service.ts
@@ -1,11 +1,12 @@
-import AppDataSource from '../../data-source'
-import { Room } from '../../entities/roomsEntity'
-
+import AppDataSource from "../../data-source";
+import { Room } from "../../entities/roomsEntity";
 
 export async function getAllRoomsService(): Promise<Room[]> {
-    const roomRepository = AppDataSource.getRepository(Room)
+  const roomRepository = AppDataSource.getRepository(Room);
 
-    const room = roomRepository.find()
+  const room = roomRepository.find({
+    relations: { movie: true, employee: true },
+  });
 
-    return room
+  return room;
 }
