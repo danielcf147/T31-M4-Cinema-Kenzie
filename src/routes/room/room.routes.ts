1034444dbb0fbe495b7f3fd8dd1fe378ca0414cd
= <danielcf.vix@hotmail.com>
2023-01-16 20:09:09 -0300

    gitignore Update

--- src/routes/room/room.routes.ts
+++ src/routes/room/room.routes.ts
@@ -3,6 +3,7 @@ import { CreateRoomController } from "../../controllers/room/createRoom.controll
 import { getAllRoomsController } from "../../controllers/room/getRoom.controller";
 import { updateRoomController } from "../../controllers/room/updateRoom.controller";
 import dataIsValid from "../../middlewares/dataIsValid.middleware";
+import ensureIsAdm from "../../middlewares/Employee/ensureIsAdm.middleware";
 import ensureAuthMiddleware from "../../middlewares/ensureAuthToken.middleware";
 import {
   createRoomSerializer,
@@ -14,12 +15,15 @@ export const roomRoute = Router();
 roomRoute.post(
   "",
   ensureAuthMiddleware,
+  ensureIsAdm,
   dataIsValid(createRoomSerializer),
   CreateRoomController
 );
 roomRoute.get("", getAllRoomsController);
 roomRoute.patch(
   "/:id",
+  ensureAuthMiddleware,
+  ensureIsAdm,
   dataIsValid(roomUpdateSerializer),
   updateRoomController
 );
