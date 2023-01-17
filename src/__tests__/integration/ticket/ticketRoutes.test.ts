8e5d6ba36f9eee3ddf34754de7888aaa00c2065f
= <danielcf.vix@hotmail.com>
2023-01-17 20:46:02 -0300

    testComplete

--- src/__tests__/integration/ticket/ticketRoutes.test.ts
+++ src/__tests__/integration/ticket/ticketRoutes.test.ts
@@ -36,9 +36,9 @@ describe("/tickets", () => {
       .post("/categories/movies")
       .send(mockedCategoryMovieCreate)
       .set("Authorization", `Bearer ${admLoginResponse.body.token}`);
-
     const categoryMovieResponse = await request(app).get("/categories/movies");
-    const categoryMovie_id = categoryMovieResponse.body.id;
+
+    const categoryMovie_id = categoryMovieResponse.body[0].id;
     mockedMovieCreate.categoryMovie_id = categoryMovie_id;
 
     await request(app)
@@ -47,7 +47,7 @@ describe("/tickets", () => {
       .set("Authorization", `Bearer ${admLoginResponse.body.token}`);
 
     const movieResponse = await request(app).get("/movies");
-    const movie_Id = movieResponse.body.id;
+    const movie_Id = movieResponse.body[0].id;
     mockedRoomCreate.movie_id = movie_Id;
 
     await request(app)
@@ -60,7 +60,7 @@ describe("/tickets", () => {
     await connection.destroy();
   });
 
-  test("POST /tickets -  Must be able to create a product", async () => {
+  test("POST /tickets -  Must be able to create a ticket", async () => {
     const userLoginResponse = await request(app)
       .post("/login")
       .send(mockedUserLogin);
@@ -72,13 +72,11 @@ describe("/tickets", () => {
     const movieResponse = await request(app).get("/movies");
     const movie_Id = movieResponse.body.id;
     mockedTicketCreate.movie_id = movie_Id;
-    console.log(movie_Id);
 
     const response = await request(app)
       .post("/tickets")
       .send(mockedTicketCreate)
       .set("Authorization", `Bearer ${userLoginResponse.body.token}`);
-    console.log(response.body);
 
     expect(response.body).toHaveProperty("id");
     expect(response.body).toHaveProperty("movie");
@@ -92,8 +90,42 @@ describe("/tickets", () => {
     expect(response.body.room).toHaveProperty("name");
     expect(response.body.room).toHaveProperty("is3D");
     expect(response.body.room).toHaveProperty("seats");
+    expect(response.body.room.seats).toBeGreaterThan(0);
     expect(response.body.movie.name).toEqual("Harry Potter");
     expect(response.body.room.is3D).toEqual(true);
     expect(response.status).toBe(201);
+  });
+
+  test("POST /tickets -  Shouldn't be able to create a ticket without authentication", async () => {
+    const roomResponse = await request(app).get("/rooms");
+    const room_Id = roomResponse.body.id;
+    mockedTicketCreate.room_id = room_Id;
+
+    const movieResponse = await request(app).get("/movies");
+    const movie_Id = movieResponse.body.id;
+    mockedTicketCreate.movie_id = movie_Id;
+
+    const response = await request(app)
+      .post("/tickets")
+      .send(mockedTicketCreate);
+
+    expect(response.body).toHaveProperty("message");
+    expect(response.status).toBe(401);
+  });
+
+  test("GET /tickets -  should be able to list all tickets", async () => {
+    const response = await request(app).get("/tickets");
+
+    expect(response.body[0]).toHaveProperty("id");
+    expect(response.body[0]).toHaveProperty("movie");
+    expect(response.body[0]).toHaveProperty("room");
+    expect(response.body[0].movie).toHaveProperty("id");
+    expect(response.body[0].movie).toHaveProperty("name");
+    expect(response.body[0].room).toHaveProperty("id");
+    expect(response.body[0].room).toHaveProperty("name");
+    expect(response.body[0].room).toHaveProperty("is3D");
+    expect(response.body[0].movie.name).toEqual("Harry Potter");
+    expect(response.body).toHaveLength(1);
+    expect(response.status).toBe(200);
   });
 });
