115a974bab2791a6adb370d6c715b81de3cc74fb
= <danielcf.vix@hotmail.com>
2023-01-17 12:31:02 -0300

    fix ticket

--- src/controllers/tickets/getAllTicketsController.ts
+++ src/controllers/tickets/getAllTicketsController.ts
@@ -2,7 +2,6 @@ import { Request, Response } from "express";
 import { getAllTicketsService } from "../../services/tickets/getAllTickets.service";
 
 export async function getAllTicketsController(req: Request, res: Response) {
-
   const ticket = await getAllTicketsService();
 
   return res.status(200).json(ticket);
--- src/services/tickets/getAllTickets.service.ts
+++ src/services/tickets/getAllTickets.service.ts
@@ -2,13 +2,21 @@ import AppDataSource from "../../data-source";
 import { Ticket } from "../../entities/ticketsEntity";
 
 export async function getAllTicketsService(): Promise<Ticket[]> {
+  const ticketRepository = AppDataSource.getRepository(Ticket);
 
-    const ticketRepository = AppDataSource.getRepository(Ticket)
+  const ticket = ticketRepository
+    .createQueryBuilder("ticket")
+    .innerJoinAndSelect("ticket.movie", "movie")
+    .innerJoinAndSelect("ticket.room", "room")
+    .select([
+      "ticket",
+      "movie.name",
+      "movie.id",
+      "room.name",
+      "room.id",
+      "room.is3D",
+    ])
+    .getMany();
 
-    const ticket = ticketRepository.find({
-        relations:{movie : true,
-        room: true}
-    })
-
-    return ticket
+  return ticket;
 }
