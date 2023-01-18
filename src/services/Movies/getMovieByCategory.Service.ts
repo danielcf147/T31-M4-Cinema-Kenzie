ed90ede6a06dfbcbc0307b8638172d9aa219e992
Flipsy1 <felipevie@hotmail.com>
2023-01-18 15:44:12 -0400

    tipagem das respostas

--- src/services/Movies/getMovieByCategory.Service.ts
+++ src/services/Movies/getMovieByCategory.Service.ts
@@ -1,7 +1,8 @@
 import AppDataSource from "../../data-source";
 import { Movie } from "../../entities/moviesEntity";
+import { IMovie } from "../../interfaces/movie/movies.Interfaces";
 
-export function getMovieByCategoryService(id: string) {
+export function getMovieByCategoryService(id: string): Promise<IMovie[]> {
   const movieRepository = AppDataSource.getRepository(Movie);
 
   const movieQueryBuilder = movieRepository.createQueryBuilder("movie");
